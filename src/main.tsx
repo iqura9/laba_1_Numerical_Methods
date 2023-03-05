1eb19fe35e5a38f2a015ec2c60f6349c4ec7f998
iqura9 <pozitiv_ON@ukr.net>
2023-03-05 19:52:09 +0200

    push

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+//created by Korniienko Oleksandr
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
 );
